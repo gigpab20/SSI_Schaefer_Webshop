64f18a72ed546ba3ddccd60ba56ecbfe3ab322fe
Jonas brunner <brujob20@htl-kaindorf.at>
2024-05-16 09:29:26 +0200

    EmailTo

--- src/elements/Grid.tsx
+++ src/elements/Grid.tsx
@@ -1,22 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {HardwareInt} from "../interface/HardwareInt";
 import HardwareCard from "./HardwareCard";
 
 interface GridProps {
-    hardware:HardwareInt[];
-    onBuy: (product:HardwareInt) => void;
+    hardware: HardwareInt[];
+    onBuy: (product: HardwareInt) => void;
 }
 
-const Grid:React.FC<{gridProps: GridProps}> = ({gridProps}) => {
-
-    const onBuy = ()=> {
-        console.log("hi");
-    }
-
+const Grid: React.FC<{ gridProps: GridProps }> = ({ gridProps }) => {
     return (
         <div className="grid grid-cols-4 gap-4">
-            {gridProps.hardware.map((item:HardwareInt) => (
-                <HardwareCard hardwareItem={{item, onBuy}}></HardwareCard>
+            {gridProps.hardware.map((item: HardwareInt) => (
+                <HardwareCard key={item.ANLAGENNR} hardwareItem={{ item, onBuy: gridProps.onBuy }} />
             ))}
         </div>
     );
