c06ac5116499421b397dbf7e7d6e7186953ea437
jbn <brujob20@htl-kaindorf.at>
2024-07-26 08:47:47 +0200

    saving

--- frontend/src/elements/Impressum.tsx
+++ frontend/src/elements/Impressum.tsx
@@ -1,15 +1,79 @@
 import React from 'react';
 import '../../src/stylesheets/contentPage.css';
 import HeaderNoSort from './HeaderNoSort';
+import { useNavigate } from 'react-router-dom';
 
 const Impressum: React.FC = () => {
+
+    const navigate = useNavigate();
+
     return (
         <>
-            <HeaderNoSort />
+            <HeaderNoSort/>
             <div className="content-container">
                 <h1>Impressum</h1>
-                <p>Dies ist der Dummy-Text für das Impressum. Hier können Sie Ihre Impressumsinformationen einfügen.</p>
+                <p><strong>SSI Schäfer</strong></p>
+                <p>
+                    SSI Schäfer AG<br/>
+                    Fritz-Schäfer-Straße 20<br/>
+                    57290 Neunkirchen<br/>
+                    Deutschland
+                </p>
+
+                <h2>Kontakt</h2>
+                <p>
+                    Telefon: +49 2735 70-1<br/>
+                    Telefax: +49 2735 70-396<br/>
+                    E-Mail: info@ssi-schaefer.com<br/>
+                    Internet: <a href="https://www.ssi-schaefer.com/de-de" target="_blank"
+                                 rel="noopener noreferrer">www.ssi-schaefer.com</a>
+                </p>
+
+                <h2>Vertreten durch</h2>
+                <p>
+                    Geschäftsführer: Steffen Bersch (CEO), Bruno Krauss (CFO), Michael Mohr (CSO), Henning Schnieder
+                    (CTO)
+                </p>
+
+                <h2>Registereintrag</h2>
+                <p>
+                    Eintragung im Handelsregister.<br/>
+                    Registergericht: Amtsgericht Siegen<br/>
+                    Registernummer: HRB 7106
+                </p>
+
+                <h2>Umsatzsteuer-ID</h2>
+                <p>
+                    Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz: DE126823944
+                </p>
+
+                <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+                <p>
+                    Steffen Bersch<br/>
+                    SSI Schäfer AG<br/>
+                    Fritz-Schäfer-Straße 20<br/>
+                    57290 Neunkirchen<br/>
+                    Deutschland
+                </p>
+
+                <h2>Streitschlichtung</h2>
+                <p>
+                    Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
+                    href="https://ec.europa.eu/consumers/odr" target="_blank"
+                    rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br/>
+                    Unsere E-Mail-Adresse finden Sie oben im Impressum.
+                </p>
+                <p>
+                    Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+                    Verbraucherschlichtungsstelle teilzunehmen.
+                </p>
+
+                <button className="button" onClick={() => navigate("/")}>
+                    Zurück
+                </button>
             </div>
+
+
         </>
     );
 };
--- frontend/src/elements/Rueckgabe.tsx
+++ frontend/src/elements/Rueckgabe.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import HeaderNoSort from './HeaderNoSort';
 import '../../src/stylesheets/contentPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const Rueckgabe: React.FC = () => {
+
+    const navigate = useNavigate();
+
     return (
         <>
             <HeaderNoSort />
             <div className="content-container">
                 <h1>Rückgabebedingungen</h1>
                 <p>
-                    Ab dem Zeitpunkt des Kaufs eines Produkts erlischt das Rückgaberecht vollständig. Dies bedeutet, dass
-                    es keine Möglichkeit gibt, das Produkt zurückzugeben, sobald der Kauf abgeschlossen ist. Bitte stellen
+                    Ab dem Zeitpunkt des Kaufs eines Produkts erlischt das Rückgaberecht vollständig. Dies bedeutet,
+                    dass
+                    es keine Möglichkeit gibt, das Produkt zurückzugeben, sobald der Kauf abgeschlossen ist. Bitte
+                    stellen
                     Sie sicher, dass Sie Ihre Kaufentscheidung sorgfältig prüfen, da nach dem Abschluss des Kaufvorgangs
                     keine Rückerstattung oder Umtausch möglich ist.
                 </p>
                 <p>
-                    Darüber hinaus sind Schäden, die durch unsachgemäße Handhabung, Missbrauch oder andere Handlungen des
-                    Käufers verursacht wurden, ebenfalls von der Rückgabe ausgeschlossen. Das bedeutet, dass keine Rückgabe
-                    oder Rückerstattung gewährt wird, wenn das Produkt aufgrund von Fahrlässigkeit, Unfällen oder absichtlicher
-                    Beschädigung durch den Käufer beschädigt wird. Wir empfehlen, die Gebrauchsanweisungen sorgfältig zu lesen
+                    Darüber hinaus sind Schäden, die durch unsachgemäße Handhabung, Missbrauch oder andere Handlungen
+                    des
+                    Käufers verursacht wurden, ebenfalls von der Rückgabe ausgeschlossen. Das bedeutet, dass keine
+                    Rückgabe
+                    oder Rückerstattung gewährt wird, wenn das Produkt aufgrund von Fahrlässigkeit, Unfällen oder
+                    absichtlicher
+                    Beschädigung durch den Käufer beschädigt wird. Wir empfehlen, die Gebrauchsanweisungen sorgfältig zu
+                    lesen
                     und zu befolgen, um solche Schäden zu vermeiden.
                 </p>
                 <p>
-                    Unsere Rückgabebedingungen stellen sicher, dass alle Kunden ihre Produkte in einwandfreiem Zustand erhalten.
-                    Falls ein Produkt jedoch bei der Lieferung beschädigt oder defekt ist, kontaktieren Sie uns bitte umgehend.
-                    In solchen Fällen werden wir eine Lösung finden, die entweder eine Reparatur, einen Ersatz oder eine Rückerstattung
+                    Unsere Rückgabebedingungen stellen sicher, dass alle Kunden ihre Produkte in einwandfreiem Zustand
+                    erhalten.
+                    Falls ein Produkt jedoch bei der Lieferung beschädigt oder defekt ist, kontaktieren Sie uns bitte
+                    umgehend.
+                    In solchen Fällen werden wir eine Lösung finden, die entweder eine Reparatur, einen Ersatz oder eine
+                    Rückerstattung
                     umfasst, je nach den Umständen des Falls und nach unserem Ermessen.
                 </p>
                 <p>
-                    Vielen Dank für Ihr Verständnis und Ihre Kooperation. Wir sind bestrebt, unseren Kunden die bestmögliche Erfahrung
+                    Vielen Dank für Ihr Verständnis und Ihre Kooperation. Wir sind bestrebt, unseren Kunden die
+                    bestmögliche Erfahrung
                     zu bieten und stehen Ihnen bei Fragen oder Anliegen jederzeit zur Verfügung.
                 </p>
+
+                <button className="button" onClick={() => navigate("/")}>
+                    Zurück
+                </button>
             </div>
         </>
     );
